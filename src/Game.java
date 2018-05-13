222cd5b2a8c42646f2d208e5c4662f723ac9dcf1
Brandon Nguyen <lrbn86@gmail.com>
2018-05-13 18:01:34 -0400

    Testing trivial functionalities

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Game {
 	
+	private static void createWindow() {
+		JFrame frame = new JFrame ("2D Chess");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel text = new JLabel ("Hello", SwingConstants.CENTER);
+		text.setPreferredSize(new Dimension(300, 100));
+		frame.getContentPane().add(text, BorderLayout.CENTER);
+		frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+
+	// Main
 	public static void main (String[] args) {
+		createWindow();
 	}
 
 }
